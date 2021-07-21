5a0b30722c7361614f33872b5b8c46a111eabfd3
SamarthBM <samarthbm18@gmail.com>
2021-07-21 12:25:09 +0530

    Initial commit

---
+++ src/com/bridgelabs/BinarySearchTreeRunner.java
@@ -0,0 +1,18 @@
+/****************************************************************
+ * Purpose : Initial commit
+ * @author Samarth BM
+
+***************************************************************/
+
+package com.bridgelabs;
+
+
+public class BinarySearchTreeRunner {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
